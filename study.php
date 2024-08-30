4156d84ab62e7f46f8c585f2c989e2b1b7607ba3
Dennis Menze <dennis.menze@fernuni-hagen.de>
2024-08-30 17:51:06 +0200

    Bugfixing

--- study.php
+++ study.php
@@ -14,39 +14,35 @@ function generateRandomString($length = 10) {
     return $randomString;
 }
 
-if(isloggedin()) {
-    require_logout();
-}
+if(!isloggedin()) {
 
-$string = generateRandomString();
-$name = $string;
-$password= $string;
+    $string = generateRandomString();
+    $name = $string;
+    $password= $string;
 
 
-$user = create_user_record($name, $password);
+    $user = create_user_record($name, $password);
+    $reason = null;
 
-print($user->username);
-print($user->password);
-$reason = null;
+    complete_user_login($user);
 
-complete_user_login($user);
+    $user->firstname = "Proband";
+    $user->lastname =(string) ($user->id);
+    $user->idnumber = (string) ($user->id);
+    $user->email = $user->username . "@example.com";
+    user_update_user($user, false, false);
 
-$user->firstname = "Proband";
-$user->lastname =(string) ($user->id);
-$user->idnumber = (string) ($user->id);
-$user->email = $user->username . "@example.com";
-user_update_user($user, false, false);
-
-$instance = null;
-$enrolinstances = enrol_get_instances(COURSE_ID, true);
-foreach ($enrolinstances as $courseenrolinstance) {
-    if ($courseenrolinstance->enrol == "manual") {
-        $instance = $courseenrolinstance;
-        break;
+    $instance = null;
+    $enrolinstances = enrol_get_instances(COURSE_ID, true);
+    foreach ($enrolinstances as $courseenrolinstance) {
+        if ($courseenrolinstance->enrol == "manual") {
+            $instance = $courseenrolinstance;
+            break;
+        }
     }
+    $enrol = enrol_get_plugin('manual');
+    $enrol->enrol_user($instance, $user->id, PARTICIPANTS_ROLE_ID, 0, 0, ENROL_USER_ACTIVE);
 }
-$enrol = enrol_get_plugin('manual');
-$enrol->enrol_user($instance, $user->id, PARTICIPANTS_ROLE_ID, 0, 0, ENROL_USER_ACTIVE);
 
 redirect(new moodle_url('/course/view.php', ['id' => COURSE_ID]));
 ?>
