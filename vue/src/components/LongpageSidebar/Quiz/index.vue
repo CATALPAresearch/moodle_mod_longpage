1c1249ae0c9a3bdda7eb122584326a40304df0f7
Dennis Menze <dennis.menze@fernuni-hagen.de>
2024-10-02 17:40:47 +0200

    Dennis Menze's avatar Enhance tooltip handling in Quiz component to prevent display on hidden elements

--- vue/src/components/LongpageSidebar/Quiz/index.vue
+++ vue/src/components/LongpageSidebar/Quiz/index.vue
@@ -342,6 +342,7 @@ export default {
               var len = 0;
               var repeat = 0;
 
+              $(".reading-progress").tooltip("dispose");
               $(".wrapper[data-reading-comprehension-count]").each(function (index, paragraph) {
                 var progress = $(paragraph).find(".reading-progress");
                 var value = parseFloat($(paragraph).attr("data-reading-comprehension-sum")) / parseInt($(paragraph).attr("data-reading-comprehension-count"));
@@ -359,8 +360,7 @@ export default {
                   ).css("opacity", Math.max(0.1, value)).addClass("reading-comprehension");
                 $(paragraph).attr("data-reading-comprehension-count", "");
               });
-
-              $(".reading-progress").tooltip("dispose").tooltip({ "placement": "auto", "html": true });
+              $(".reading-progress").tooltip({ "placement": "auto", "html": true });
 
               var rc = 0;
               if (len > 0) {
@@ -1515,6 +1515,25 @@ export default {
           type: ev.type, 
         };
         log("moved", logentry);
+      });
+
+      $('[data-toggle="tooltip"]').on('show.bs.tooltip', function () {
+         // Überprüfen, ob das Element selbst und alle übergeordneten Elemente sichtbar sind
+        var $element = $(this);
+        var isVisible = $element.is(':visible') && $element.css('opacity') > 0;
+
+        // Übergeordnete Elemente durchlaufen, um deren Sichtbarkeit zu überprüfen
+        $element.parents().each(function() {
+            if ($(this).is(':hidden') || $(this).css('opacity') === '0') {
+                isVisible = false; // Wenn ein übergeordnetes Element versteckt oder opak ist, nicht sichtbar
+                return false; // Stoppe die Schleife
+            }
+        });
+
+        // Wenn das Element oder ein übergeordnetes Element nicht sichtbar ist, verhindere das Anzeigen des Tooltips
+        if (!isVisible) {
+            return false; // Verhindert die Anzeige des Tooltips
+        }
       });
 
       function changeLockButton(locked)
